3ccadf47ee7923780fdb4f56a73e01b5cab1745e
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-04-09 12:51:37 +0300

    Extra files deleted

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,11 @@
-import React, { Fragment,useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Landing from './components/Landing';
-import Register from './components/Register';
-import Login from './components/Login';
-import Alert from './components/Alert';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
-// Redux
-import { Provider } from 'react-redux';
-import store from './store';
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
-
-const App = () => {
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
+function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Route exact path='/' component={Landing} />
-          <section>
-          <Alert/>
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router>
-    </Provider>
+    <div>
+      <h1>Hello Team-2</h1>
+    </div>
   );
-  
 }
 
 export default App;
