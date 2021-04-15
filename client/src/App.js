be189a3a2e406957bb76812481ec8f61d5e2911c
Valuriel <christian.poetter@hotmail.de>
2021-04-15 14:20:23 +0300

    added Decision component and implemented its route to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,45 @@
+import React, { Fragment,useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Landing from './components/Landing';
+import Footer from './components/Footer';
+import Register from './components/Register';
+import Login from './components/Login';
+import Alert from './components/Alert';
+import Decision from './components/Decision';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
-function App() {
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
-    <div>
-      <h1>Hello Chris</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Route exact path='/' component={Landing} />
+          <section>
+          <Alert/>
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/decision' component={Decision} />
+            </Switch>
+          </section>
+          <Footer/>
+        </Fragment>
+      </Router>
+    </Provider>
   );
+  
 }
 
 export default App;
