5efda81e4f856522bbdb670e3ce530b7b8ff0e45
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-05-05 12:23:19 +0300

    Feature/Chat front end ready

--- client/src/components/chat/ChatCompany.js
+++ client/src/components/chat/ChatCompany.js
@@ -2,23 +2,18 @@ import React from 'react';
 import './Chat.css';
 import { Link } from 'react-router-dom';
 
-const Chat = ({
-    auth,
-  profile
-}) => {
-
-  const room = `${profile.user._id.toString()}-${auth.user._id.toString()}`;
+const ChatCompany = ({ auth, profile }) => {
+  const room = `${auth.user._id.toString()}-${profile.user._id.toString()}`;
   return (
     <Link to={`/chat?name=${auth.user.name}&room=${room}`}>
       <div className='chat'>
         <img className='chat__image' src={profile.logo || profile.image} />
         <div className='chat__details'>
           <h2>{profile.user.name}</h2>
-
         </div>
       </div>
     </Link>
   );
 };
 
-export default Chat;
+export default ChatCompany;
