e1b15299d681af78dd2ae7d3448f5999dd41f198
KaleKemosabe <kale.kemosabe@gmail.com>
2021-05-06 14:34:44 +0300

    add alert popup styling

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import './Alert.scss';
 
 const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 &&
   alerts.map((alert) => (
-    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-      {alert.msg}
+    <div className="alert-container">
+      <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+        <p className="alert-icon"><i class="fas fa-exclamation-circle fa-3x"></i></p>
+        <p className="alert-msg">{alert.msg}</p>
+      </div>
     </div>
   ));
 
