d954f55ae40b46ea404396c47033661b6968003f
Aaron Ojala <aaronojala@yahoo.com>
2021-05-07 13:18:33 +0300

    Revert "add alert popup styling"
    This reverts commit e1b15299d681af78dd2ae7d3448f5999dd41f198.

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import './Alert.scss';
 
 const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 &&
   alerts.map((alert) => (
-    <div className="alert-container">
-      <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-        <p className="alert-icon"><i class="fas fa-exclamation-circle fa-3x"></i></p>
-        <p className="alert-msg">{alert.msg}</p>
-      </div>
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      {alert.msg}
     </div>
   ));
 
