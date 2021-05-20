d8cf7757eac2bd3a0e8da80755b706a5d7359a25
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-05-20 10:48:23 +0300

    trying for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -15,6 +16,17 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/companyprofile', require('./routes/api/companyprofile'));
 app.use('/api/employeeprofile', require('./routes/api/employeeprofile'));
+
+//Serve statis assets in production
+if(process.env.NODE_ENV==='production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 
 const PORT = process.env.PORT || 5000;
 
