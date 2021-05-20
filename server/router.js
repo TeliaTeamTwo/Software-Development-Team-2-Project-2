f42da969f7f1ba1d0c7ba44cc0ea8c4fef68c10b
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-05-20 10:19:38 +0300

    Chat server deleted

--- server/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const collection = require('./models/Chat');
-
-router.get("/", (req, res) => {
-  res.send({ response: "Server is up and running." }).status(200);
-});
-
-module.exports = router;
