b121a68bc3806f4795864d8b4924f660755c3e13
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-05-20 11:37:23 +0300

    gitignore changes

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/companyprofile', require('./routes/api/companyprofile'));
 app.use('/api/employeeprofile', require('./routes/api/employeeprofile'));
 
+//Serve statis assets in production
+if(process.env.NODE_ENV==='production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
